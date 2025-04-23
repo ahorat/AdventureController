fcd9a4585c5c6f460c0f3de2bb5be964555ccdb2
ahorat <147364667+ahorat@users.noreply.github.com>
2025-04-23 22:45:50 +0200

    Added LEDs to code and added arrow keys as second keyset

--- sw/src/main.cpp
+++ sw/src/main.cpp
@@ -2,21 +2,27 @@
 #include <Bounce2.h>
 #include <bluefruit.h>
 
-#define DEBUG
+//#define DEBUG
 
 #define BUTTON_PRESSED LOW
 #define BUTTON_RELEASED HIGH
 #define BOUNCE_INTERVAL 5
 
 #define NUM_BUTTONS 4
-#define NUMBER_OF_PROFILES 2
-#define ID_OF_CONSUMER_PROFILE 1
+#define NUMBER_OF_PROFILES 3
+#define ID_OF_ARROW_PROFILE 1
+#define ID_OF_CONSUMER_PROFILE 2
 
 #define PROFILE_SWITCH_BUTTON_1 0
 #define PROFILE_SWITCH_BUTTON_2 3
 #define PROFILE_SWITCH_INTERVAL 5000
 
 #define MULTIPRESS_INTERVAL 500
+
+#define LED_GREEN_1_PIN PIN_017
+#define LED_RED_PIN  PIN_020
+#define LED_BLUE_PIN PIN_008
+#define LED_GREEN_2_PIN PIN_006
 
 BLEDis bledis;
 BLEHidAdafruit blehid;
@@ -27,9 +33,18 @@ uint32_t nextKeyStrikeTimeMs[NUM_BUTTONS];
 const int BUTTON_PINS[NUM_BUTTONS] = {PIN_031, PIN_029, PIN_002, PIN_115};
 
 const uint16_t KEY_CODES_PROFILE_1[NUM_BUTTONS] = {'=', '-', 'r', 'c'};
-const uint16_t KEY_CODES_PROFILE_2[NUM_BUTTONS] = {HID_USAGE_CONSUMER_SCAN_NEXT, HID_USAGE_CONSUMER_SCAN_PREVIOUS,
-                                                   HID_USAGE_CONSUMER_VOLUME_DECREMENT, HID_USAGE_CONSUMER_VOLUME_INCREMENT};
-const uint16_t *KEY_CODES[NUMBER_OF_PROFILES] = {KEY_CODES_PROFILE_1, KEY_CODES_PROFILE_2};
+const uint16_t KEY_CODES_PROFILE_2[NUM_BUTTONS] = {HID_KEY_ARROW_RIGHT, HID_KEY_ARROW_LEFT,
+                                                   HID_KEY_ARROW_UP, HID_KEY_ARROW_DOWN};
+const uint16_t KEY_CODES_PROFILE_3[NUM_BUTTONS] = {HID_USAGE_CONSUMER_SCAN_NEXT, HID_USAGE_CONSUMER_SCAN_PREVIOUS,
+                                                   HID_USAGE_CONSUMER_VOLUME_INCREMENT, HID_USAGE_CONSUMER_VOLUME_DECREMENT};
+const uint16_t *KEY_CODES[NUMBER_OF_PROFILES] = {KEY_CODES_PROFILE_1, KEY_CODES_PROFILE_2, KEY_CODES_PROFILE_3};
+
+const int LED_PINS[4] = {
+    LED_GREEN_1_PIN,
+    LED_GREEN_2_PIN,
+    LED_BLUE_PIN,
+    LED_RED_PIN
+};
 
 uint32_t currentProfile;
 bool profileChanged;
@@ -38,6 +53,30 @@ void fireKey(uint32_t buttonId);
 
 void setup()
 {
+  pinMode(LED_BLUE_PIN, OUTPUT);
+  pinMode(LED_RED_PIN, OUTPUT);
+  pinMode(LED_GREEN_1_PIN, OUTPUT);
+  pinMode(LED_GREEN_2_PIN, OUTPUT);
+
+  digitalWrite(LED_BLUE_PIN, 0);
+  digitalWrite(LED_RED_PIN, 0);
+  digitalWrite(LED_GREEN_1_PIN, 0);
+  digitalWrite(LED_GREEN_2_PIN, 0);
+
+  delay(1000);
+  digitalWrite(LED_BLUE_PIN, 1);
+  digitalWrite(LED_RED_PIN, 1);
+  digitalWrite(LED_GREEN_1_PIN, 1);
+  digitalWrite(LED_GREEN_2_PIN, 1);
+
+  delay(1000);
+  digitalWrite(LED_BLUE_PIN, 0);
+  digitalWrite(LED_RED_PIN, 0);
+  digitalWrite(LED_GREEN_1_PIN, 0);
+  digitalWrite(LED_GREEN_2_PIN, 0);
+
+  delay(1000);
+
   currentProfile = 0;
   profileChanged = false;
 
@@ -93,6 +132,10 @@ void setup()
 
 void loop()
 {
+  digitalWrite(LED_GREEN_1_PIN, Bluefruit.connected());
+  digitalWrite(LED_GREEN_2_PIN, currentProfile == 1 ? 1 : 0);
+  digitalWrite(LED_BLUE_PIN, currentProfile == 2 ? 1 : 0);
+
   delay(5);
   for (int i = 0; i < NUM_BUTTONS; i++)
   {
@@ -156,6 +199,12 @@ void fireKey(uint32_t button_id)
   {
     blehid.consumerKeyPress(KEY_CODES[currentProfile][button_id]);
     blehid.consumerKeyRelease();
+  }
+  else if(currentProfile == ID_OF_ARROW_PROFILE)
+  {
+    uint8_t keycodes[6] = {(uint8_t)(KEY_CODES[currentProfile][button_id]), HID_KEY_NONE, HID_KEY_NONE, HID_KEY_NONE, HID_KEY_NONE, HID_KEY_NONE};
+    blehid.keyboardReport(0, keycodes);
+    blehid.keyRelease();
   }
   else
   {
